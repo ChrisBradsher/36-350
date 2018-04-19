a3144b930f7beb8d84e7a47483b13b46457248ba
Christopher Bradsher <cbradshe@andrew.cmu.edu>
2018-04-19 00:07:03 -0400

    Part 2b model p-values

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,3 +4,14 @@ generate_data = function(n,p){
   list(covariates,responses)
 }
 
+model_select = function(covariates, responses, cutoff){
+  model=lm(responses ~ covariates)
+  pvals = summary(model)$coefficients[,4]
+  keep=which(pvals<cutoff)-1
+  keep=keep[keep!=0]
+  if(length(keep)==0){
+    return(c())
+  }
+  model2=lm(responses ~ covariates[,keep])
+  summary(model2)$coefficients[,4]
+}
